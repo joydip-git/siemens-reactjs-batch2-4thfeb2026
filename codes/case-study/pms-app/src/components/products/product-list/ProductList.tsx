d7fab62db2805d6ac13adff0640fcba758d6c6c2
joydip-git <joydip.mondal@gmail.com>
2026-02-04 14:55:02 +0530

    product list created

--- codes/case-study/pms-app/src/components/products/product-list/ProductList.tsx
+++ codes/case-study/pms-app/src/components/products/product-list/ProductList.tsx
@@ -1,17 +1,43 @@
+import { useState } from "react"
+import type { Product } from "../../../models/product"
+import { productRecords } from "../../../data/products"
+
 const ProductList = () => {
-    const names = ['anil', 'sunil']
+    const [products, setProducts] = useState<Product[]>(productRecords)
     return (
         <div>
-            <ul>
-                {
-                    names
-                        .map(
-                            (name) => {
-                                return <li>{name}</li>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map(
+                            (p) => {
+                                return (
+                                    <tr>
+                                        <td>
+                                            <img
+                                                src={p.imageUrl}
+                                                alt="NA"
+                                                title={p.productName}
+                                                style={{ width: '80px', margin: '2px' }} />
+                                        </td>
+                                        <td>{p.productName}</td>
+                                        <td>{p.price}</td>
+                                        <td>{p.starRating}</td>
+                                    </tr>
+                                )
                             }
                         )
-                }
-            </ul>
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
